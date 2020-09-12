1fd07f7db77b81a4730eeb8be1f9fc05d4d4a439
yoyohoho84 <kirill_1131@mail.ru>
2020-09-12 17:49:42 +0300

    Updates

---
+++ precache-manifest.37d024b230d96aadd730fe1efc018828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122d98ec5f1f3cfc489a817dfa35b16f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcb019b6e072be289c4f",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "37a90fea1ea9a318640f",
+    "url": "./static/js/2.3a9eb52f.chunk.js"
+  },
+  {
+    "revision": "7c6de3322eb4ea2ab32454492afd4f37",
+    "url": "./static/js/2.3a9eb52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb019b6e072be289c4f",
+    "url": "./static/js/main.c6ccd112.chunk.js"
+  },
+  {
+    "revision": "be112707c8e765be0a9d",
+    "url": "./static/js/runtime-main.94d52426.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export * from './MenuApp';
-export * from './Table';
